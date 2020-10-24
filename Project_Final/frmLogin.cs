a9236aa46eb3821817ce0019cd9e165c410703c0
hieulm67 <lmhieu0607@gmail.com>
2020-10-24 12:33:52 +0700

    Change structure

--- Project_Final/frmLogin.cs
+++ Project_Final/frmLogin.cs
@@ -1,6 +1,4 @@
-﻿using Project_Final.Ado;
-using Project_Final.Bussiness;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,47 +8,19 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Project_Final
-{
-    public partial class frmLogin : Form
-    {
+namespace Project_Final {
+    public partial class frmLogin : Form {
         public int Role { get; set; }
 
-        public frmLogin()
-        {
+        public frmLogin() {
             InitializeComponent();
         }
 
-        private void btnSumbit_Click(object sender, EventArgs e)
-        {
-            string username = txtUsername.Text;
-            string password = txtPassword.Text;
+        private void btnSumbit_Click(object sender, EventArgs e) {
 
-            //Show error dialog whether 1 or 2 field is empty
-            if (username.Equals("") || password.Equals(""))
-            {
-                MessageBox.Show("Please input all the field !!!");
-                return;
-            }
-
-            //Call DAO to check login
-            StaffDAO staffDAO = new StaffDAO();
-
-            StaffDTO staffDTO = staffDAO.CheckLogin(username, password);
-            if(staffDTO != null)
-            {
-                this.Role = staffDTO.Role;
-                this.Close();
-            }
-            else
-            {
-                MessageBox.Show("Wrong user !!");
-            }
-            
         }
 
-        private void btnCancel_Click(object sender, EventArgs e)
-        {
+        private void btnCancel_Click(object sender, EventArgs e) {
             this.Close();
         }
     }
