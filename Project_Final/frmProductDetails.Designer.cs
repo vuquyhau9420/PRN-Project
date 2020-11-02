27f2a1b8281347aa70bb685daac1dd16b344c130
vuquyhau9420 <vuquyhau09042000@gmail.com>
2020-11-02 17:20:23 +0700

    Add ucMakeOrder, add ucProduct into frmProductDetails

---
+++ Project_Final/frmProductDetails.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Project_Final
+{
+    partial class frmProductDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.contentPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // contentPanel
+            // 
+            this.contentPanel.Location = new System.Drawing.Point(-1, 1);
+            this.contentPanel.Name = "contentPanel";
+            this.contentPanel.Size = new System.Drawing.Size(799, 453);
+            this.contentPanel.TabIndex = 0;
+            this.contentPanel.SizeChanged += new System.EventHandler(this.contentPanel_SizeChanged);
+            // 
+            // frmProductDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.contentPanel);
+            this.Name = "frmProductDetails";
+            this.Text = "Product Details";
+            this.Load += new System.EventHandler(this.frmProductDetails_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel contentPanel;
+    }
+}
---
+++ Project_Final/frmProductDetails.cs
@@ -0,0 +1,32 @@
+﻿using Project_Final.ucControl;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Final
+{
+    public partial class frmProductDetails : Form
+    {
+        ucProductFrm ucProductFrm = new ucProductFrm();
+        public frmProductDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void frmProductDetails_Load(object sender, EventArgs e)
+        {
+            contentPanel.Controls.Add(ucProductFrm);
+        }
+
+        private void contentPanel_SizeChanged(object sender, EventArgs e)
+        {
+            ucProductFrm.Size = contentPanel.Size;
+        }
+    }
+}
