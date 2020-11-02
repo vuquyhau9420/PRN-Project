143ef46967f64fcb57d2019c2113e3fbbf40b783
hieulm67 <lmhieu0607@gmail.com>
2020-11-02 11:52:35 +0700

    Add store procedure for productMainFrm

--- Store Procedure.sql
+++ Store Procedure.sql
@@ -1,13 +1,46 @@
-CREATE PROC spCheckLogin(@STAFF_USERNAME VARCHAR(100), @STAFF_PASSWORD VARCHAR(30))
+CREATE PROCEDURE spCheckLogin(@STAFF_USERNAME VARCHAR(100), @STAFF_PASSWORD VARCHAR(30))
 AS
 	BEGIN
 		SELECT staff_id, staff_username, staff_password, staff_fullname, staff_citizen_identification, staff_sex, staff_phone, staff_address, staff_birthday, staff_role, staff_status
         FROM staff 
         WHERE staff_username = @STAFF_USERNAME 
-              AND staff_password = @STAFF_PASSWORD AND staff_status = 'TRUE'
+              AND staff_password = @STAFF_PASSWORD AND staff_status = 1
 	END
 GO
 
 DROP PROC spCheckLogin
 
 EXEC spCheckLogin @STAFF_USERNAME = 'hieulm', @STAFF_PASSWORD = '123456'
+
+CREATE PROCEDURE spGetCategorys
+AS
+	BEGIN
+		SELECT category_id, category_name, category_status
+        FROM category 
+        WHERE category_status = 1
+	END
+GO
+
+EXEC spGetCategorys
+
+CREATE PROCEDURE spGetProductGroup(@CATEGORY_ID int)
+AS
+	BEGIN
+		SELECT product_group_id, product_group_name, product_group_catgory, product_group_stocking, product_group_status
+        FROM product_group 
+        WHERE product_group_status = 1 AND product_group_catgory = @CATEGORY_ID
+	END
+GO
+
+EXEC spGetProductGroup @CATEGORY_ID = 1
+
+CREATE PROCEDURE spGetProducts(@PRODUCT_GROUP_ID varchar(10))
+AS
+	BEGIN
+		SELECT product_group_id, product_id, product_name, product_quantity, product_import_price, product_sale_price, product_description, product_image, product_status
+        FROM product 
+        WHERE product_status = 1 AND product_group_id = @PRODUCT_GROUP_ID
+	END
+GO
+
+EXEC spGetProducts AP06
